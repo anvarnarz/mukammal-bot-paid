ca48901b53db922d7a9ed7163ccca49e89e04e9a
Anvar Narzullaev <anvarbek@gmail.com>
2021-09-22 11:38:58 +0800

    Deploy to Heroku

--- data/config.py
+++ data/config.py
@@ -1,11 +1,7 @@
-from environs import Env
-
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+import os
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
-PROVIDER_TOKEN = env.str("PROVIDER_TOKEN")
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
+ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
+IP = str(os.environ.get("ip"))  # Xosting ip manzili
+PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
