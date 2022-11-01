1f19fae5c0d02ad8059f6b0dbfc8df28c87913f3
Ahmadjon <79581200+ahmadjon-coder-creator@users.noreply.github.com>
2022-11-01 20:53:22 +0500

    Update service_messages.py

--- handlers/groups/service_messages.py
+++ handlers/groups/service_messages.py
@@ -1,21 +1,25 @@
+from aiogram.dispatcher.filters import AdminFilter
+from loader import dp, bot
 from aiogram import types
 
-from filters import IsGroup
-from loader import dp, bot
+@dp.message_handler(content_types=["new_chat_members"]) #Зашел
+async def on_user_joined(message: types.Message):
+    await message.delete()
 
 
-@dp.message_handler(IsGroup(), content_types=types.ContentType.NEW_CHAT_MEMBERS)
-async def new_member(message: types.Message):
-    members = ", ".join([m.get_mention(as_html=True) for m in message.new_chat_members])
-    await message.reply(f"Xush kelibsiz, {members}.")
+@dp.message_handler(content_types=["left_chat_member"]) #Вышел
+async def on_user_exit(message: types.Message):
+    await message.delete()
 
+@dp.message_handler(content_types=['text'])
+async def delete_links(message: types.Message):
+    admins_list = [admin.user.id for admin in await bot.get_chat_administrators(chat_id=message.chat.id)]
+    if message.from_user.id not in admins_list:
 
-@dp.message_handler(IsGroup(), content_types=types.ContentType.LEFT_CHAT_MEMBER)
-async def banned_member(message: types.Message):
-    if message.left_chat_member.id == message.from_user.id:
-        await message.answer(f"{message.left_chat_member.get_mention(as_html=True)} guruhni tark etdi")
-    elif message.from_user.id == (await bot.me).id:
-        return
-    else:
-        await message.answer(f"{message.left_chat_member.full_name} guruhdan haydaldi "
-                             f"Admin: {message.from_user.get_mention(as_html=True)}.")
+        if '@' in message.text: #Удаление сообщений с тегами (@тег)
+            await bot.delete_message(message.chat.id, message.message_id)
+
+        for entity in message.entities: #Удаление сообщений с ссылками
+            if entity.type in ["url", "text_link"]:
+                await bot.delete_message(message.chat.id, message.message_id)
+                await bot.send_message(message.chat.id, "Agar siz e'lonlaringiz va havolalar yubormoqchi bo'lsangiz, administrator @m_m_ahmadjon bilan bog'laning")
