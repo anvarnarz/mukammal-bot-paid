f3b1ea33a9eca66e3a1ccc4b901b07448bef78b0
Izzatillo <64713970+izzatillo-egamberdiyev@users.noreply.github.com>
2022-12-19 22:04:22 -0800

    Update notify_admins.py

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -2,13 +2,5 @@ import logging
 
 from aiogram import Dispatcher
 
-from data.config import ADMINS
-
-
 async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
-        try:
-            await dp.bot.send_message(admin, "Bot ishga tushdi")
-
-        except Exception as err:
-            logging.exception(err)
+    print("Bot ishga tushdi")
