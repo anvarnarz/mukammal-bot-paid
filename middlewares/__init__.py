0b227f1ad6ab6828395c2dde3e6d920122452567
Izzatillo <64713970+izzatillo-egamberdiyev@users.noreply.github.com>
2022-12-19 22:06:37 -0800

    Update __init__.py

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,8 +1,1 @@
-from aiogram import Dispatcher
 
-from loader import dp
-from .throttling import ThrottlingMiddleware
-
-
-if __name__ == "middlewares":
-    dp.middleware.setup(ThrottlingMiddleware())
