21dd471eaa05fd826d9cd03ef1b2cb5f85180ec1
Izzatillo <64713970+izzatillo-egamberdiyev@users.noreply.github.com>
2022-12-19 22:17:00 -0800

    Create Database.py

---
+++ utils/db_api/Database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import insert, select, update
+
+class UserRepo(BaseSQLAlchemyRepo):
+    model = User
+
+    async def add_user(self, user_id: int, name: str, language: str, is_subscribed: bool, phone: str) -> None:
+        sql = insert(self.model).values(
+            user_id=user_id,
+            name=name,
+            language=language,
+            is_subscribed=is_subscribed,
+            phone=phone
+        )
+        await self._session.execute(sql)
+        await self._session.commit()
