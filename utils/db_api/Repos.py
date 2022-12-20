5ca7b01684fe4b3b5d90b6a92c80a3314279dc5d
Izzatillo <64713970+izzatillo-egamberdiyev@users.noreply.github.com>
2022-12-19 22:36:57 -0800

    Delete Repos.py

--- utils/db_api/Repos.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-
-from sqlalchemy.ext.asyncio import AsyncSession
-
-
-class BaseSQLAlchemyRepo(ABC):
-    def __init__(self, session: AsyncSession) -> None:
-        self._session = session
