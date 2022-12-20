1ab043108498a40f2d263f86ed3fd907a0c39f25
Izzatillo <64713970+izzatillo-egamberdiyev@users.noreply.github.com>
2022-12-19 22:39:44 -0800

    Update Database.py

--- utils/db_api/Database.py
+++ utils/db_api/Database.py
@@ -1,1 +1,6 @@
+import aiomysql
 
+async def test_example():
+    conn = await aiomysql.connect(host=localhost, port=3306,
+                                  user='root', password='', db='mysql')
+    conn.close()
