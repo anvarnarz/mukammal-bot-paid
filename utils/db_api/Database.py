e466ccb1e2a6438b09b365e886f69188e2c696d2
Izzatillo <64713970+izzatillo-egamberdiyev@users.noreply.github.com>
2022-12-19 22:46:33 -0800

    Update Database.py

--- utils/db_api/Database.py
+++ utils/db_api/Database.py
@@ -4,3 +4,11 @@ async def connection():
     conn = await aiomysql.connect(host=localhost, port=3306,
                                   user='root', password='', db='mysql')
     conn.close()
+   
+async def user_info(chat_id):
+    connect = await connection()
+    async with connect.cursor() as cursor:
+        await cursor.execute(f"SELECT FROM `user` WHERE `chat_id` = {chat_id}")
+        print(cursor)
+        r = await cursor.fetchall()
+        print(r)
