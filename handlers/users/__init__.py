de7b69834f753ac8fe661485bf9c9c61c20f7638
Amirjoin <amirjonkarimoff@gmail.com>
2023-10-08 10:42:45 +0500

    keyboart button

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,4 +1,5 @@
 from . import help
 from . import start
 from . import anketa
+from . import menu
 from . import echo
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,11 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
+from keyboards.default.start import menuStart
 
 from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer(f"Salom, {message.from_user.full_name}!\n")
+    await message.answer(f"Telefoningiz va manzilingizni yuboring:",reply_markup=menuStart)
